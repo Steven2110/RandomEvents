f57b70e32188d045f92289a999f19a5bf138c5b1
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-05-04 19:46:12 +0700

    Initial Commit

---
+++ RandomEvents/RandomEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomEventsApp.swift
+//  RandomEvents
+//
+//  Created by Steven Wijaya on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
